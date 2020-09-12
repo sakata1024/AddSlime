4dd5b3be6e7c3a6853e81ea80e659c299d824e59
Matts966 <Matts966@users.noreply.github.com>
2020-09-12 23:30:18 +0900

    Add stage factory

--- Assets/matsui/Scripts/StageFactory.cs
+++ Assets/matsui/Scripts/StageFactory.cs
@@ -4,10 +4,21 @@ using UnityEngine;
 
 public class StageFactory : MonoBehaviour
 {
+    public Stage stage;
+    public int stageSizeX;
+    public int stageSizeY;
+    public GameObject iceBlock;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        for (var x = 0; x < stageSizeX; x++)
+        {
+            for (var y = 0; y < stageSizeY; y++)
+            {
+                Instantiate(iceBlock, new Vector3(x - stageSizeX / 2, y - stageSizeY / 2, 0), Quaternion.identity);
+            }
+        }
     }
 
     // Update is called once per frame
