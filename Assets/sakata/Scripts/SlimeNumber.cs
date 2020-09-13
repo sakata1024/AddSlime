6606a34caa24e9c5d58335b69496c3d3206070de
sakata1024 <skt97yuma@outlook.jp>
2020-09-13 12:05:00 +0900

    スライムに数字が出るように変更

---
+++ Assets/sakata/Scripts/SlimeNumber.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SlimeNumber : MonoBehaviour
+{
+    public Slime slime;
+    public Text numberText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var num = slime.number;
+        numberText.text = num.ToString();
+        if(num >= 11)
+        {
+            numberText.color = Color.white;
+        }
+    }
+}
